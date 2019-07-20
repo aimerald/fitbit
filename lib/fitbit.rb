8bb998f4625d37a1be9c38fd8fc94c102b82c4df
ug <aimerald3@gmail.com>
2019-07-20 20:41:16 +0900

    並列でログを取得できるようにした

--- lib/fitbit.rb
+++ lib/fitbit.rb
@@ -1,20 +1,21 @@
 require "fitbit/version"
-Dir.glob('./lib/fitbit/log/*') do |file|
-  require file
-end
 require 'pry'
 require 'active_support'
 require 'active_support/core_ext/string'
+require 'parallel'
+Dir.glob('./lib/fitbit/log/*') do |file| require file end
 
 module Fitbit
   class Error < StandardError; end
-
   def self.fetch_all_logs
-    files = Dir.glob('./lib/fitbit/log/*').map do |file|
-      File.basename(file, '.rb').camelize
-    end - ['Base']
-    files.each do |file|
+    Parallel.each(log_files, in_threads: 4) do |file|
       "Fitbit::Log::#{file}".constantize.get!(date: '2019-07-19').export_json
     end
+  end
+
+  def self.log_files
+    Dir.glob('./lib/fitbit/log/*').map do |file|
+      File.basename(file, '.rb').camelize
+    end - ['Base']
   end
 end
