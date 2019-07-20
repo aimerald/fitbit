7c3872a7d9af7c5f5fe5b144a676e0e90a667879
ug <aimerald3@gmail.com>
2019-07-20 20:37:54 +0900

    各種ログを一度に取得するメソッドの追加

--- lib/fitbit.rb
+++ lib/fitbit.rb
@@ -3,13 +3,18 @@ Dir.glob('./lib/fitbit/log/*') do |file|
   require file
 end
 require 'pry'
+require 'active_support'
+require 'active_support/core_ext/string'
 
 module Fitbit
   class Error < StandardError; end
-  # Your code goes here...
 
-  def self.test
-    data = Fitbit::Log::Sleep.get!(date: '2019-07-19')
-    data.export_json
+  def self.fetch_all_logs
+    files = Dir.glob('./lib/fitbit/log/*').map do |file|
+      File.basename(file, '.rb').camelize
+    end - ['Base']
+    files.each do |file|
+      "Fitbit::Log::#{file}".constantize.get!(date: '2019-07-19').export_json
+    end
   end
 end
